fe7eda1b91443416ca561b7fdff82cd03c0f31a8
PavelSB <1987PavelS@mail.ru>
2024-01-20 18:28:48 +0300

    bot changed

--- ScrumBoard_bot/bot_main.py
+++ ScrumBoard_bot/bot_main.py
@@ -7,60 +7,49 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardRemove, Message, CallbackQuery
 from bot_mfuncs import sing_in, tasks_load, descr_load, task_change
 
-"""
-Запись логов в файл с уровнем инфо:
-"""
+
+# Запись логов в файл с уровнем инфо:
 logging.basicConfig(level=logging.INFO, filename='bot_logging.txt', filemode='a',  #'utf-8',
                     format='%(asctime)s: %(levelname)s %(funcName)s-%(lineno)d %(message)s')
 
-"""
-Присвоение значения токена константе:
-"""
+# Присвоение значения токена константе:
 with open('key.txt', 'r') as f:
     text = f.readline()
 TOKEN = str(text)
-"""
-Регуляно используемое сообщение присвоено константе:
-"""
+# Регуляно используемое сообщение присвоено константе:
 MSG = '{}, choose an action'
 
-"""
-Создание бота и диспетчера для него:
-"""
+# Создание бота и диспетчера для него:
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot, storage=MemoryStorage())
 
-"""
-Словари для временного хранения информации о пользователях онлайн:
-"""
+# Словари для временного хранения информации о пользователях онлайн:
 bot_users_online = {} # ключи: id пользователя бота; значения: логин, пароль, статус, акцесс-токен.
 bot_users_tasks = {}  # ключи: all, my; значения: все задачи, задачи пользователя.
-bot_users_change = {} # ключ: id задачи; значение: новый статус задачи.
+bot_users_change = {} # ключи: id пользователя; значения: ключ: id задачи, значение: новый статус задачи.
 
 
-"""
-Класс онлайн пользователя для машины состояний:
-"""
 class Form(StatesGroup):
-    first_msg = State()      # запрос логина пользователя (для удаления его из чата)
-    second_msg = State()     # логин, ответ пользователя (для удаления его из чата)
-    third_msg = State()      # запрос пароля пользлвателя (для удаления его из чата)
-    fourth_msg = State()     # пароль, ответ пользователя (для удаления его из чата)
+    """Класс онлайн пользователя для машины состояний:"""
+    first_msg = State()      # запрос логина пользователя (для удаления сообщения из чата)
+    second_msg = State()     # логин, ответ пользователя (для удаления сообщения из чата)
+    third_msg = State()      # запрос пароля пользователя (для удаления сообщения из чата)
+    fourth_msg = State()     # пароль, ответ пользователя (для удаления сообщения из чата)
     login_user = State()     # логин, для записи в словарь bot_users_online
     password_user = State()  # пароль, для записи в словарь bot_users_online
     tasks_id_user = State()  # id из базы данных, для записи в словарь bot_users_tasks
     tasks_all_user = State() # задачи из базы данных, для записи в словарь bot_users_tasks
 
 
-"""
-Хендлер команды старт:
-запись в логи о том, кто вызвал команду;
-запись пользователя в словарь bot_users_online;
-приветственное сообщение бота пользователю с обращением по имени;
-отправка регуляно используемого сообщения ботом с кнопками авторизации и выхода.
-"""
 @dp.message_handler(commands=['start'])
 async def start_handler(message: Message):
+    """
+    Хендлер команды старт:
+    - запись в логи о том, кто вызвал команду;
+    - запись пользователя в словарь bot_users_online;
+    - приветственное сообщение бота пользователю с обращением по имени;
+    - отправка регуляно используемого сообщения ботом с кнопками авторизации и выхода.
+    """
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     user_full_name = message.from_user.full_name
@@ -72,15 +61,15 @@ async def start_handler(message: Message):
     await message.answer(MSG.format(user_name), reply_markup=mks.sing_buttons)
 
 
-"""
-Хендлер команды выход:
-команда сработает только если пользователь задавал команду старт;
-запись в логи о вызове команды;
-удаление всей информации о пользователе в словарях временного хранения данных о пользователях онлайн;
-бота прощается с пользователем и удаляет кнопки меню.
-"""
 @dp.message_handler(commands=['quit'])
 async def quit_handler(message: Message):
+    """
+    Хендлер команды выход:
+    - команда сработает только если пользователь ранее задавал команду старт;
+    - запись в логи о вызове команды;
+    - удаление всей информации о пользователе в словарях временного хранения данных о пользователях онлайн;
+    - бота прощается с пользователем и удаляет Keyboard кнопки.
+    """
     user_id = message.from_user.id
     if user_id in bot_users_online:
         msg_id = message.message_id
@@ -92,15 +81,15 @@ async def quit_handler(message: Message):
         await message.answer('Goodbye! See you...', reply_markup=ReplyKeyboardRemove())
 
 
-"""
-Хендлер команды логин:
-запись в логи о вызове команды;
-если пользователь онлайн - открывается машина состояний;
-бот запрашивает логин, запоминает первое сообщение для дальнейшего удаления;
-передача состояния к экземпляру класса Form - логин.
-"""
 @dp.message_handler(commands=['login'])
 async def login_handler(message: Message, state: FSMContext):
+    """
+    Хендлер команды логин:
+    - запись в логи о вызове команды;
+    - если пользователь онлайн - открывается машина состояний;
+    - бот запрашивает логин, запоминает первое сообщение для дальнейшего удаления;
+    - передача состояния к экземпляру класса Form - логин.
+    """
     user_id = message.from_user.id
     if user_id in bot_users_online:
         msg_id = message.message_id
@@ -113,15 +102,15 @@ async def login_handler(message: Message, state: FSMContext):
         await Form.login_user.set()
 
 
-"""
-Хендлер ответа на запрос логина:
-запись в логи о вызове команды;
-фиксация ответного сообщения с логином пользователя, плюс - для дальнейшего удаления;
-бот запрашивает пароль, запоминает и это сообщение для дальнейшего удаления;
-передача состояния к экземпляру класса Form - пароль.
-"""
 @dp.message_handler(state=Form.login_user)
 async def login_text(message: Message, state: FSMContext):
+    """
+    Хендлер ответа на запрос логина:
+    - запись в логи о вызове команды;
+    - фиксация ответного сообщения с логином пользователя, а также для дальнейшего удаления;
+    - бот запрашивает пароль, запоминает и это сообщение для дальнейшего удаления;
+    - передача состояния к экземпляру класса Form - пароль.
+    """
     user_id = message.from_user.id
     msg_id = message.message_id
     lgn = message.text
@@ -136,17 +125,17 @@ async def login_text(message: Message, state: FSMContext):
     await Form.password_user.set()
 
 
-"""
-Хендлер ответа на запрос пароля:
-запись в логи о вызове команды;
-фиксация ответного сообщения с паролем пользователя;
-бот удаляет четыре сообщения из чата в целях информационной безопасности
-(удаляются запросы логина и пароля, а также сами сообщения с логином и паролем пользователя);
-бот сообщает, что принял от пользователя логин и длину паролья;
-передача состояния к экземпляру класса Form - id пользователя в базе данных.
-"""
 @dp.message_handler(state=Form.password_user)
 async def password_text(message: Message, state: FSMContext):
+    """
+    Хендлер ответа на запрос пароля:
+    - запись в логи о вызове команды;
+    - фиксация ответного сообщения с паролем пользователя;
+    - бот удаляет четыре, ранее запомненных, сообщения из чата в целях информационной безопасности
+    (удаляются запросы логина и пароля, а также сами сообщения с логином и паролем пользователя);
+    - бот сообщает, что принял от пользователя логин и пароль, указывая только его длину;
+    - передача состояния к экземпляру класса Form - id пользователя в базе данных.
+    """
     user_name = message.from_user.first_name
     user_id = message.from_user.id
     msg_id = message.message_id
@@ -164,6 +153,7 @@ async def password_text(message: Message, state: FSMContext):
     await message.answer(f'Data accepted:\n{lgn}, {new_text}')
     await Form.tasks_id_user.set()
 
+    response_sing = sing_in(data['login_user'], data['password_user'])
     """
     1. Запрос к базе данных с логином и паролем для аутентификации пользователя:
     - если аутентификация положительная:
@@ -173,16 +163,7 @@ async def password_text(message: Message, state: FSMContext):
     - если аутентификация отрицательная:
     бот информирует об этом пользователя, в логи записывается статус ошибки аутентификации,
     логин-статус пользователя присваевается False, машина состояний закрывается.
-    2. После аутентификации отправляется запрос на авторизацию пользователя к базе данных:
-    - если авторизация положительная:
-    в логи записываются задачи из базы данных, бот сообщает об успешной авторизации,
-    в словарь bot_users_tasks записываются два словаря - все задачи, мои задачи,
-    машина состояний закрывается, бот отправляет регулярное сообшение и меняет кнопку логин на меню;
-    - если авторизация отрицательная:
-    бот информирует об этом пользователя, в логи записывается статус ошибки авторизации,
-    просит пользователя воспользоваться кнопками меню, машина состояний закрывается.
     """
-    response_sing = sing_in(data['login_user'], data['password_user'])
     if response_sing.status_code == 200:
         # Аутентификация прошла успешно:
         logging.info(f'{user_id=}, Successful authorization')
@@ -209,6 +190,16 @@ async def password_text(message: Message, state: FSMContext):
         await state.finish()
         await message.answer('The login or password is entered incorrectly,\nor such user does not exist')
     
+    """
+    2. После аутентификации отправляется запрос на авторизацию пользователя к базе данных:
+    - если авторизация положительная:
+    в логи записываются задачи из базы данных, бот сообщает об успешной авторизации,
+    в словарь bot_users_tasks записываются два словаря - все задачи, мои задачи,
+    машина состояний закрывается, бот отправляет регулярное сообшение и меняет кнопку логин на меню;
+    - если авторизация отрицательная:
+    бот информирует об этом пользователя, в логи записывается статус ошибки авторизации,
+    просит пользователя воспользоваться кнопками меню, машина состояний закрывается.
+    """
     if bot_users_online[user_id]['login_status'] == True:
         logging.info(f'{user_id=}, Data is loading...')
         response_tasks = tasks_load(access_token)
@@ -238,23 +229,22 @@ async def password_text(message: Message, state: FSMContext):
         logging.info(f'{user_id=}, message: user is not authorized')
 
 
-"""
-Хендлер команды меню:
-проверка - пользователь онлайн, логин-статус True,
-если - да, то запись в логи о вызове команды, появляется меню с инлайн-кнопками;
-если - нет, или что-то пошло не по логике, то бот сообщает об этом пользователю
-и меняет кнопки на изначальные - догин и выход.
-
-"""
 @dp.message_handler(commands=['menu'])
 async def options_handler(message: Message):
+    """
+    Хендлер команды меню:
+    - проверка - пользователь онлайн, логин-статус True;
+    - если - да, то запись в логи о вызове команды, появляется меню с инлайн-кнопками;
+    - если - нет, или что-то пошло не по логике, то бот сообщает об этом пользователю
+    и меняет кнопки на изначальные - логин и выход.
+    """
     user_id = message.from_user.id
     if user_id in bot_users_online:
         if bot_users_online[user_id]['login_status'] == True:
             msg_id = message.message_id
             user_msg = message.text
             logging.info(f'{user_id=}, {msg_id=}, {user_msg=}')
-            await message.answer('Select from the menu:', reply_markup=mks.keyboard)
+            await message.answer('Select from the menu:', reply_markup=mks.create_keyboard_inline())
         else:
             await message.answer('You are not authenticated', reply_markup=mks.sing_buttons)
     else:
@@ -262,38 +252,37 @@ async def options_handler(message: Message):
                              reply_markup=mks.sing_buttons)
 
 
-"""
-Хендлер на любое сообщение от пользователя:
-бот отвечает, что - не понимает команду и, что - у него есть конкретный алгоритм.
-"""
 @dp.message_handler()
 async def data_handler(message: Message):
+    """
+    Хендлер на любое сообщение от пользователя:
+    - бот отвечает, что - не понимает команду и, что - у него есть конкретный алгоритм.
+    """
     await message.answer("I don't understand you,\nthis is not included in my algorithm")
 
 
-"""
-Хендлер работы с инлайн-кнопками:
-1) две кнопки - все задачи, мои задачи,
-при нажатии на все задачи - выводится соответствеющий список,
-при нажатии на мои задачи - выводится список кнопок по количеству задач;
-2) после выбора задачи из списка - поялвяется описание самой задачи,
-а под ней две кнопки - изменить статус и мои задачи;
-3) при нажатии на изменить статус - выводится список фиксированных статусов из STATUSES,
-после выбора статуса в словарь bot_users_change записывается id задачи и новый её статус,
-далее появляются две кнопки - подтвердить и отменить;
-4) при нажатии кнопки подтвердить - отправляется запрос к базе данных на изменение статуса задачи,
-при нажатии кнопки отменить - информация в словаре bot_users_change стерается;
-5) если в базе данных изменения произошли успешно, из неё выгружаются обновленные данные и
-перезаписываются в словаре bot_users_tasks, о чём бот сообщает пользователю,
-если в базе данных изменения не произошли - информация в словаре bot_users_change стерается,
-бот предлагает пользователю начать процедуру заново;
-6) каждый этап кол-бэка логируется, при возникновении поломки алгоритма меню инлайн-кнопок
-стерается информация о пользователе во всех словарях временного хранения информации и
-бот предлагает начать всё заново, появляются кнопки логин и выход.
-"""
 @dp.callback_query_handler(lambda call: True)
 async def callback_options(query: CallbackQuery):
-
+    """
+    Хендлер работы с инлайн-кнопками:
+    1) две кнопки - все задачи, мои задачи,
+    при нажатии на все задачи - выводится список всех задач всех пользователей,
+    при нажатии на мои задачи - выводится список кнопок по количеству задач пользователя;
+    2) после выбора задачи из списка - поялвяется описание самой задачи,
+    а под ней две кнопки - изменить статус и мои задачи;
+    3) при нажатии на изменить статус - выводится список фиксированных статусов из STATUSES,
+    после выбора статуса в словарь bot_users_change записывается id задачи и новый её статус,
+    далее появляются две кнопки - подтвердить и отменить;
+    4) при нажатии кнопки подтвердить - отправляется запрос к базе данных на изменение статуса задачи,
+    при нажатии кнопки отменить - информация в словаре bot_users_change стерается;
+    5) если в базе данных изменения произошли успешно, из неё выгружаются обновленные данные и
+    перезаписываются в словаре bot_users_tasks, о чём бот сообщает пользователю,
+    если в базе данных изменения не произошли - информация в словаре bot_users_change стерается,
+    бот предлагает пользователю начать процедуру заново;
+    6) каждый этап кол-бэка логируется, при возникновении поломки алгоритма меню инлайн-кнопок
+    стерается информация о пользователе во всех словарях временного хранения информации и
+    бот предлагает начать всё заново, появляются кнопки логин и выход.
+    """
     data = query.data
     user_id = query.from_user.id
     msg_id=query.message.message_id
@@ -340,7 +329,7 @@ async def callback_options(query: CallbackQuery):
             await bot.send_message(chat_id=query.message.chat.id, text=result)
             logging.info(f'{user_id=}, {data} have been sent')
             await bot.send_message(chat_id=query.message.chat.id, text=f'{MSG.format(user_name)}:',
-                                   reply_markup=mks.keyboard_my_tasks)
+                                   reply_markup=mks.create_keyboard_my_tasks())
             bot_users_change[user_id] = dict({'task_id': task_using[5:]})
 
     elif data == 'change status':
@@ -351,7 +340,7 @@ async def callback_options(query: CallbackQuery):
             logging.info(f'{user_id=}, message: You have no current tasks')
         else:
             await bot.edit_message_text(chat_id=user_id, message_id=msg_id, text='select a task status:',
-                                        reply_markup=await mks.create_keyboard_my_task(STATUSES))
+                                        reply_markup=await mks.create_keyboard_edit_task(STATUSES))
             logging.info(f'{user_id=}, message: Start changing task status')
 
     elif data in STATUSES:
@@ -364,7 +353,7 @@ async def callback_options(query: CallbackQuery):
         else:
             await bot.edit_message_text(chat_id=user_id, message_id=msg_id,
                                         text=f'confirm changes: status -> {data}',
-                                        reply_markup=mks.keyboard_to_status)
+                                        reply_markup=mks.create_keyboard_to_status())
             logging.info(f'{user_id=}, message: Selected status to change')
             bot_users_change[user_id].update({'status': status})
 
@@ -401,7 +390,7 @@ async def callback_options(query: CallbackQuery):
                 bot_users_change[user_id] = None
                 await bot.edit_message_text(chat_id=user_id, message_id=msg_id,
                                             text='Task status changed successfully',
-                                            reply_markup=mks.keyboard)
+                                            reply_markup=mks.create_keyboard_inline())
                 logging.info(f'{user_id=}, message: Task status changed successfully')
             else:
                 value = 'Something went wrong, try again'
@@ -417,7 +406,7 @@ async def callback_options(query: CallbackQuery):
             bot_users_change[user_id] = None
             await bot.edit_message_text(chat_id=user_id, message_id=msg_id,
                                         text='Select from the menu:',
-                                        reply_markup=mks.keyboard)
+                                        reply_markup=mks.create_keyboard_inline())
             logging.info(f'{user_id=}, message: Action canceled')
 
     else:
@@ -430,8 +419,6 @@ async def callback_options(query: CallbackQuery):
         logging.info(f'{user_id=}, message: Unknown request')
 
 
-"""
-Запиуск телеграм-бота
-"""
 if __name__ == '__main__':
+    """Запуск телеграм-бота"""
     executor.start_polling(dp, skip_updates=True)
--- ScrumBoard_bot/bot_markups.py
+++ ScrumBoard_bot/bot_markups.py
@@ -1,32 +1,35 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-"""
-Контекстные кнопки: логин - выход, меню - выход.
-"""
-s_btns = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-m_btns = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
 
-btn_lgn = KeyboardButton('/login')
-btn_out = KeyboardButton('/quit')
-btn_opt = KeyboardButton('/menu')
+async def create_keyboard(data):
+    """Контекстные кнопки: логин - выход, меню - выход"""
+    s_btns = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    m_btns = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
 
-sing_buttons = s_btns.add(btn_lgn, btn_out)
-menu_buttons = m_btns.add(btn_opt, btn_out)
+    btn_lgn = KeyboardButton('/login')
+    btn_out = KeyboardButton('/quit')
+    btn_men = KeyboardButton('/menu')
 
+    if data == "login":
+        return s_btns.add(btn_lgn, btn_out)
+    else:
+        return m_btns.add(btn_men, btn_out)
 
-"""
-Инлайн-кнопки: все задачи, мои задачи.
-"""
-keyboard = InlineKeyboardMarkup()
-keyboard.row(InlineKeyboardButton('all tasks', callback_data='all tasks'),
-             InlineKeyboardButton('my tasks', callback_data='my tasks'))
+sing_buttons = create_keyboard("login")
+menu_buttons = create_keyboard("menu")
 
 
-"""
-Инлайн-кнопки: список задач из мои задачи.
-"""
+async def create_keyboard_inline():
+    """Инлайн-кнопки: все задачи, мои задачи"""
+    keyboard = InlineKeyboardMarkup()
+    keyboard.row(InlineKeyboardButton('all tasks', callback_data='all tasks'),
+                InlineKeyboardButton('my tasks', callback_data='my tasks'))
+    return keyboard
+
+
 async def create_keyboard_tasks(tasks_short):
+    """Инлайн-кнопки: список задач из мои задачи"""
     keyboard_tasks = InlineKeyboardMarkup(row_width=1)
     keys_list = tasks_short.keys()
     for i in keys_list:
@@ -34,29 +37,25 @@ async def create_keyboard_tasks(tasks_short):
     return keyboard_tasks
 
 
-
-"""
-Инлайн-кнопки: сменить статус - мои задачи.
-"""
-keyboard_my_tasks = InlineKeyboardMarkup()
-keyboard_my_tasks.row(InlineKeyboardButton('change status', callback_data='change status'),
-                      InlineKeyboardButton('my tasks', callback_data='my tasks'))
-
+async def create_keyboard_my_tasks():
+    """Инлайн-кнопки: сменить статус - мои задачи"""
+    keyboard_my_tasks = InlineKeyboardMarkup()
+    keyboard_my_tasks.row(InlineKeyboardButton('change status', callback_data='change status'),
+                        InlineKeyboardButton('my tasks', callback_data='my tasks'))
+    return keyboard_my_tasks
 
 
-"""
-Инлайн-кнопки: список статусов для задачи.
-"""
-async def create_keyboard_my_task(statuses):
+async def create_keyboard_edit_task(statuses):
+    """Инлайн-кнопки: список статусов для задачи"""
     keyboard_task_edit = InlineKeyboardMarkup(row_width=1)
     for i in statuses:
         keyboard_task_edit.add(InlineKeyboardButton(f'{i}', callback_data=f'{i}'))
     return keyboard_task_edit
 
 
-"""
-Инлайн-кнопки: подтвердить - отменить.
-"""
-keyboard_to_status = InlineKeyboardMarkup()
-keyboard_to_status.row(InlineKeyboardButton('confirm', callback_data='confirm'),
-                      InlineKeyboardButton('cancel', callback_data='cancel'))
+async def create_keyboard_to_status():
+    """Инлайн-кнопки: подтвердить - отменить"""
+    keyboard_to_status = InlineKeyboardMarkup()
+    keyboard_to_status.row(InlineKeyboardButton('confirm', callback_data='confirm'),
+                        InlineKeyboardButton('cancel', callback_data='cancel'))
+    return keyboard_to_status
